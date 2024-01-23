262b348c279725afd28fbcbc059681364d22cb26
Suhas-Reddy-S <143859218+Suhas-Reddy-S@users.noreply.github.com>
2024-01-23 00:39:02 -0700

    Updated writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #Author: Suhas-Reddy-S
 
-writefile=$1
+#Variables to hold fisrt and second cli arguements
+writefile=$1 
 writestr=$2
 
 if [ "$#" -ne 2 ]
 then
   echo "Total Number of arguements should be equal to two. First arguement is the file path and Second arguement is the String to be added to the file."
   exit 1
-else 
+else
+  mkdir -p "$(dirname "${writefile}")"
   echo "${writestr}" > "${writefile}"
   if [ $? -ne 0 ]
   then
