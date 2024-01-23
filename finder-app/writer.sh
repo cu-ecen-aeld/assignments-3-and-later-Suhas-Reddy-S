ab03a9b6b682875fa4cc71a65691111294842871
Suhas-Reddy-S <143859218+Suhas-Reddy-S@users.noreply.github.com>
2024-01-23 00:55:18 -0700

    Commented writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
-#Author: Suhas-Reddy-S
+# Author: Suhas-Reddy-S
 
-#Variables to hold fisrt and second cli arguements
+# Variables to hold first and second CLI arguments
 writefile=$1 
 writestr=$2
 
-if [ "$#" -ne 2 ]
+# Check if the number of arguments is not equal to 2
+if [ "$#" -ne 2 ] 
 then
-  echo "Total Number of arguements should be equal to two. First arguement is the file path and Second arguement is the String to be added to the file."
+  echo "Total number of arguments should be equal to two. The first argument is the file path, and the second argument is the string to be added to the file."
   exit 1
 else
+  # Create the directory (if not exists) for the file
   mkdir -p "$(dirname "${writefile}")"
+
+  # Write the string to the file
   echo "${writestr}" > "${writefile}"
-  if [ $? -ne 0 ]
+
+  # Check if the file creation was successful
+  if [ $? -ne 0 ] 
   then
-    echo "Error creating a file"
+    echo "Error creating the file"
     exit 1
   fi
 fi
-
