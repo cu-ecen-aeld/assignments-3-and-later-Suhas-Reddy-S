1ec681e453f4c54339933e1fa6a33a170a4cfe9b
Suhas-Reddy-S <143859218+Suhas-Reddy-S@users.noreply.github.com>
2024-01-28 20:24:29 -0700

    Updated finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Author: Suhas-Reddy-S
 
 # Variables to hold first and second CLI arguments
@@ -10,19 +10,17 @@ if [ "$#" -ne 2 ]
 then
   echo "Total number of arguments should be equal to two. First argument is the directory path and the second argument is the string to be searched."
   exit 1
-else 
-  # Check if the first argument is a directory; if not, print an error message
-  if [ ! -d "${filesdir}" ] 
-  then    
-    echo "Error: The specified directory '${filesdir}' doesn't exist. Please enter a valid path."
-    exit 1
-  else 
-    # Get the number of files in the directory
-    num_of_files=$(find "${filesdir}" -type f | wc -l)
+fi 
+# Check if the first argument is a directory; if not, print an error message
+if [ ! -d "${filesdir}" ] 
+then    
+  echo "Error: The specified directory '${filesdir}' doesn't exist. Please enter a valid path."
+  exit 1
+fi 
+# Get the number of files in the directory
+num_of_files=$(ls -r "${filesdir}" | wc -l)
 
-    # Get the number of occurrences of the search string
-    num_of_occurrences=$(grep -R "${searchstr}" "${filesdir}" | wc -l)
+# Get the number of occurrences of the search string
+num_of_occurrences=$(grep -r "${searchstr}" "${filesdir}" | wc -l)
 
-    echo "The number of files is ${num_of_files} and the number of occurrences is ${num_of_occurrences}."
-  fi
-fi
+echo "The number of files are  ${num_of_files} and the number of matching lines are ${num_of_occurrences}."
