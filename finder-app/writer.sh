8eb16aa1ec746f59bca7bf83149182c8bab8ff7a
Suhas-Reddy-S <143859218+Suhas-Reddy-S@users.noreply.github.com>
2024-01-28 20:25:44 -0700

    Updated writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Author: Suhas-Reddy-S
 
 # Variables to hold first and second CLI arguments
@@ -10,29 +10,30 @@ if [ "$#" -ne 2 ]
 then
   echo "Total number of arguments should be equal to two. The first argument is the file path, and the second argument is the string to be added to the file."
   exit 1
+fi
+# Extract the directory path from the provided file path
+dir_path="$(dirname "${writefile}")"
+
+# Check if the directory already exists
+if [ ! -d "$dir_path" ] 
+then 
+  # Create the directory if it doesn't exist
+  mkdir -p "$dir_path"
+fi
+
+# Check if the specified path is a directory
+if [ -d "$writefile" ] 
+then
+  echo "Error: The specified path is a directory. Please provide a valid file path."
+  exit 1
 else
-  # Extract the directory path from the provided file path
-  dir_path="$(dirname "${writefile}")"
-
-  # Check if the directory already exists
-  if [ ! -d "$dir_path" ]; then
-    # Create the directory if it doesn't exist
-    mkdir -p "$dir_path"
-  fi
-
-  # Check if the specified path is a directory
-  if [ -d "$writefile" ]; then
-    echo "Error: The specified path is a directory. Please provide a valid file path."
-    exit 1
-  fi
-
   # Write the string to the file
   echo "${writestr}" > "${writefile}"
+  exit 0
+fi
 
-  # Check if the file creation was successful
-  if [ $? -ne 0 ] 
-  then
-    echo "Error creating the file"
-    exit 1
-  fi
+# Check if the file creation was successful
+if [ $? -ne 0 ] 
+then
+  echo "Error creating the file"
 fi
